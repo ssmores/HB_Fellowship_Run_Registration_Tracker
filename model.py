a7a494b05ceed2efc01b19e73763fd6457e904f4
Stefanie Moy <ssmores@hotmail.com>
2016-08-16 08:20:09 +0000

    Updated model.py, base and homepage html files.

--- model.py
+++ model.py
@@ -14,10 +14,10 @@ class User(db.Model):
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     user_fname = db.Column(db.String(200), nullable=False)
     user_lname = db.Column(db.String(200), nullable=False)
-    user_email = db.Column(db.String(400), nullable=False)
+    user_email = db.Column(db.String(600), nullable=False)
     password = db.Column(db.String(64), nullable=False)
 
-    tracked_race = db.relationship('Tracked_Race')
+    tracked_races = db.relationship('Tracked_Race')
 
     def __repr__(self):
         """Provide user's first and last name."""
@@ -31,25 +31,27 @@ class Race(db.Model):
 
     race_id = db.Column(db.String(400), primary_key=True)
     event_name = db.Column(db.String(2000), nullable=False)
-    # The start date contains a unicode datetime, so recording the timezone offset and timezone DTS hours is needed.
-    event_date = db.Column(db.String(100), nullable=True)
+    # Start date is datetime, so recording the timezone offset and timezone DTS hours is needed.
+    event_date = db.Column(db.DateTime, nullable=True)
     event_tzone_offset = db.Column(db.Integer, nullable=True)
     event_tzone_DST = db.Column(db.Integer, nullable=True)
     event_city = db.Column(db.String(200), nullable=True)
     event_state = db.Column(db.String(100), nullable=True)
-    event_zipcode = db.Column(db.String(10), nullable=True)
-    event_lat = db.Column(db.String(200), nullable=True)
-    event_lng = db.Column(db.String(200), nullable=True)
-    event_distance = db.Column(db.String(200), nullable=True)
+    event_zipcode = db.Column(db.String(20), nullable=True)
+    event_lat = db.Column(db.Integer, nullable=True)
+    event_lng = db.Column(db.Integer, nullable=True)
+    # Remove the event_distance.
+    # Need an associative table, that has race_id, distrance_type_id, and the association_id.
+    # Need a distance_type table.
+    # event_distance = db.Column(db.String(200), nullable=True)
     event_url = db.Column(db.String(1000), nullable=True)
 
-    tracked_race = db.relationship('Tracked_Race')
+    tracked_races = db.relationship('Tracked_Race')
 
     def __repr__(self):
         """Provides name, distance, and event date."""
 
-        return "<Race name: %s, distance: %s, at %s.>" % (self.event_name, 
-            self.event_distance, self.event_date)
+        return "<Race name: %s, at %s.>" % (self.event_name, self.event_date)
 
 
 class Tracked_Race(db.Model):
@@ -60,13 +62,13 @@ class Tracked_Race(db.Model):
     tracked_race_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     race_id = db.Column(db.String(400), db.ForeignKey('races.race_id'))
-    registered_status_indicator = db.Column(db.Boolean, nullable=True) 
-    hotel_reserved_indicator = db.Column(db.Boolean, nullable=True)
-    airfare_reserved_indicator = db.Column(db.Boolean, nullable=True)
-    transportation_reserved_indicator = db.Column(db.Boolean, nullable=True)
+    registered_status_indicator = db.Column(db.Boolean, default=False) 
+    hotel_reserved_indicator = db.Column(db.Boolean, default=False)
+    airfare_reserved_indicator = db.Column(db.Boolean, default=False)
+    transportation_reserved_indicator = db.Column(db.Boolean, default=False)
     email_notification_start_date_at = db.Column(db.DateTime, nullable=True)
     email_notification_end_date_at = db.Column(db.DateTime, nullable=True)
-    email_freqency = db.Column(db.Integer, nullable=True)
+    email_interval = db.Column(db.Integer, nullable=True)
 
     user = db.relationship('User')
     race = db.relationship('Race')
@@ -84,16 +86,42 @@ class Email_Transaction(db.Model):
 
     transaction_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     tracked_race_id = db.Column(db.Integer, db.ForeignKey('tracked_races.tracked_race_id'))
-    start_date_at = db.Column(db.DateTime, nullable=True)
-    need_subsequent_email_indicator = db.Column(db.Boolean, nullable=True)
-    next_schedule_email_at = db.Column(db.DateTime, nullable=True)
-
+    email_date = db.Column(db.DateTime, nullable=True)
+    need_subsequent_email_indicator = db.Column(db.Boolean, default=False)
+    
     def __repr__(self):
         """Email transaction record."""
 
         return "<First email sent: %s. Another email coming: %s>" % (
-            self.start_date_at, self.need_subsequent_email_indicator)
+            self.email_date, self.need_subsequent_email_indicator)
+
+class Distance_Type(db.Model):
+    """A race distance."""
+
+    __tablename__ = 'distance_types'
+
+    distance_type_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    distance_length = db.Column(db.String(100), nullable=True)
     
+    def __repr__(self):
+        """Information on distance length."""
+
+        return "<The %s distance length.>" % (self.distance_length)
+
+
+class Race_Distance_Type(db.Model):
+    """Information on a race's distance(s)."""
+
+    __tablename__ = 'race_distance_types'
+
+    race_distance_type_id = db.Column(db.Integer, 
+                                      autoincrement=True, 
+                                      primary_key=True)
+    race_id = db.Column(db.Integer, db.ForeignKey('races.race_id'))
+    distance_type_id = db.Column(db.Integer, 
+                                 db.ForeignKey('distance_types.distance_type_id'))
+
+
 
 
 #########################################################################
